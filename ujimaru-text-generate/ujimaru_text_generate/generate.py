a5a7e9055ddeaad752379095bf68ece15c9048bb
hppRC <hpp.ricecake@gmail.com>
2020-05-26 21:13:38 +0900

    :sparkles: YATTAZE(texts are successfully generated)

--- ujimaru-text-generate/ujimaru_text_generate/generate.py
+++ ujimaru-text-generate/ujimaru_text_generate/generate.py
@@ -15,151 +15,73 @@ from sentencepiece import SentencePieceProcessor
 
 import subprocess
 
-
 # %%
-# print(subprocess.run("tree"))
+sentencepice_dir = os.path.expanduser('ujimaru_text_generate/sentencepiece')
+reformer_dir = os.path.expanduser('ujimaru_text_generate/reformer')
+
+with open(os.path.join(sentencepice_dir, "wiki-daimyo.txt")) as f:
+    text = f.read().strip()
 
 # %%
 TOKENIZER = SentencePieceProcessor()
-TOKENIZER.load('ujimaru_text_generate/sentencepiece/wiki-daimyo.model')
-
-
-# %%
-# IDS = TOKENIZER.EncodeAsIds(text)
-# IDS = onp.asarray(IDS, dtype=onp.int32)
-# print("Number of tokens:", IDS.shape[0])
-
+TOKENIZER.load(os.path.join(sentencepice_dir, 'wiki-daimyo.model'))
 
 # %%
+IDS = TOKENIZER.EncodeAsIds(text)
+IDS = onp.asarray(IDS, dtype=onp.int32)
+print("Number of tokens:", IDS.shape[0])
+
 # Configure hyperparameters.
-gin.parse_config("""
-import trax.layers
-import trax.models
-import trax.optimizers
-import trax.supervised.inputs
-import trax.supervised.trainer_lib
-
-# Parameters that will vary between experiments:
-# ==============================================================================
-train.model = @trax.models.ReformerLM
-# Our model will have 6 layers, alternating between the LSH attention proposed
-# in the Reformer paper and local attention within a certain context window.
-n_layers = 6
-attn_type = [
-  @TimeBinCausalAttention,
-  @LSHCausalAttention,  
-  @TimeBinCausalAttention,
-  @LSHCausalAttention,
-  @TimeBinCausalAttention,
-  @LSHCausalAttention,
-  ]
-share_qk = False  # LSHCausalAttention ignores this flag and always shares q & k
-n_heads = 2
-attn_kv = 64
-dropout = 0.05
-n_tokens = 524288
-
-# Parameters for MultifactorSchedule:
-# ==============================================================================
-MultifactorSchedule.constant = 0.01
-MultifactorSchedule.factors = 'constant * linear_warmup * cosine_decay'
-MultifactorSchedule.warmup_steps = 100
-MultifactorSchedule.steps_per_cycle = 900
-
-# Parameters for Adam:
-# ==============================================================================
-Adam.weight_decay_rate=0.0
-Adam.b1 = 0.86
-Adam.b2 = 0.92
-Adam.eps = 1e-9
-
-# Parameters for TimeBinCausalAttention:
-# ==============================================================================
-TimeBinCausalAttention.bin_length = 64
-TimeBinCausalAttention.dropout = 0.05
-TimeBinCausalAttention.n_bins = None
-TimeBinCausalAttention.share_qk = %share_qk
-
-# Parameters for LSHCausalAttention:
-# ==============================================================================
-LSHCausalAttention.allow_duplicate_attention = False
-LSHCausalAttention.attend_across_buckets = True
-LSHCausalAttention.rehash_each_round = True
-LSHCausalAttention.data_rotation = False
-LSHCausalAttention.n_bins = 4096
-LSHCausalAttention.n_buckets = 8192
-LSHCausalAttention.factorize_hash = [64, 128]
-LSHCausalAttention.n_hashes = 1
-LSHCausalAttention.one_rng = False
-LSHCausalAttention.hard_k = 0
-LSHCausalAttention.dropout = 0.0
-LSHCausalAttention.drop_for_hash_rate = 0.0
-LSHCausalAttention.max_len_for_inference = 2048
-LSHCausalAttention.bucket_capacity_for_inference = 64
-
-# Parameters for ReformerLM:
-# ==============================================================================
-ReformerLM.attention_type = %attn_type
-ReformerLM.d_attention_key = %attn_kv
-ReformerLM.d_attention_value = %attn_kv
-ReformerLM.d_model = 256
-ReformerLM.d_ff = 512
-ReformerLM.dropout = %dropout
-ReformerLM.ff_activation = @trax.layers.Relu
-ReformerLM.max_len = %n_tokens
-ReformerLM.mode = 'train'
-ReformerLM.n_heads = %n_heads
-ReformerLM.n_layers = %n_layers
-ReformerLM.vocab_size = 320
-ReformerLM.share_qk = %share_qk
-ReformerLM.axial_pos_shape = (512, 1024)
-ReformerLM.d_axial_pos_embs= (64, 192)
-""")
-# %%
-output_dir = os.path.expanduser('ujimaru_text_generate/reformer')
-
-
-# %%
-
-# trainer = trax.supervised.Trainer(
-#     model=trax.models.ReformerLM,
-#     loss_fn=trax.layers.CrossEntropyLoss,
-#     optimizer=trax.optimizers.Adam,
-#     lr_schedule=trax.lr.MultifactorSchedule,
-#     inputs=trax.supervised.inputs.Inputs(my_inputs),
-#     output_dir=output_dir,
-#     has_weights=True)
-
+gin.parse_config_file('ujimaru_text_generate/config.gin')
 
 # %%
 
 # As we report in the Reformer paper, increasing the number of hashing rounds
 # helps with quality. We can even increase the number of hashing rounds at
 # evaluation time only.
-# gin.parse_config("""LSHCausalAttention.n_hashes = 4""")
-# model_infer = trax.models.ReformerLM(mode='predict')
-
-# %%
-
-# Initialize model for inference.
-# gin.parse_config("""LSHCausalAttention.n_hashes = 4""")
+gin.parse_config("""LSHCausalAttention.n_hashes = 4""")
 model_infer = trax.models.ReformerLM(mode='predict')
-print(model_infer)
-
-
-# %%
-
 
 # Set up the initial state for sampling.
 initial_state = model_infer.new_weights_and_state(
     trax.supervised.trainer_lib.ShapeDtype((1, 1), dtype=np.int32))[1]
-print(initial_state)
-
-
-# %%
 
 # load pretrained weight
-model_infer.init_from_file("ujimaru_text_generate/reformer/model.pkl")
+model_infer.init_from_file(os.path.join(
+    reformer_dir, "model.pkl"), weights_only=True)
+
+print(trax.math.device_count())
 
 
-# %%
+def prediction(length=2048, prompt=None,):
+    """Sample from the ReformerLM model"""
+    # Token id 0 is the equivalent of a "start" token
+    model_infer.state = initial_state             # stateの初期化
+    cur_inputs = np.zeros((1, 1), dtype=np.int32)  # 初期値=0の挿入
+    all_samples = []
+    if prompt is not None:
+        prompt = np.asarray(TOKENIZER.EncodeAsIds(prompt))
+
+    for iteration in range(length):
+        logits = model_infer(cur_inputs)
+
+        if prompt is not None and iteration < prompt.shape[0]:
+            cur_samples = onp.array(prompt[iteration], dtype=int)
+        else:
+            logits = onp.array(logits)[0, 0, :]
+            probs = onp.exp(logits)
+            cur_samples = onp.random.choice(probs.shape[-1], p=probs[:])
+            cur_samples = onp.array(cur_samples, dtype=int)
+
+        all_samples.append(cur_samples)
+        cur_inputs = np.array(cur_samples[None, None])
+
+    all_samples = onp.stack(all_samples, -1)
+
+    return all_samples
+
+
+prefix = [5, 3, 5, 2, 1, 6]
+pred = prediction(10, "家康は")
+
+print(TOKENIZER.DecodeIds(pred.tolist()))

3c20fcb28e5f5c1d608f2e174db86b0118011aff
hppRC <hpp.ricecake@gmail.com>
2020-05-26 09:52:40 +0900

    :sparkles: Add package

--- ujimaru-text-generate/ujimaru_text_generate/__init__.py
+++ ujimaru-text-generate/ujimaru_text_generate/__init__.py
@@ -1,1 +1,15 @@
 __version__ = '0.1.0'
+
+import requests
+import sys
+
+
+def get(country: str) -> str:
+    url = f"https://corona-stats.online/{country}?minimal=true"
+    response = requests.get(url, headers={'user-agent': 'curl'})
+    return response.text
+
+
+def main() -> None:
+    country = sys.argv[1] if len(sys.argv) > 1 else ""
+    print(get(country))

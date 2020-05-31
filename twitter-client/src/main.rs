4add92edcd636304111fb34ba640b5cf163d631e
hppRC <hpp.ricecake@gmail.com>
2020-05-31 16:35:38 +0900

    :tada: Got it

--- twitter-client/src/main.rs
+++ twitter-client/src/main.rs
@@ -1,17 +1,23 @@
 use anyhow::Result;
 
-#[tokio::main]
-async fn main() -> Result<()> {
+async fn get_api_client() -> Result<kuon::TwitterAPI> {
     let access_token = &std::env::var("ACCESS_TOKEN")?;
     let access_token_secret = &std::env::var("ACCESS_TOKEN_SECRET")?;
     let api_key = &std::env::var("API_KEY")?;
     let api_secret_key = &std::env::var("API_SECRET_KEY")?;
 
-    let api: kuon::TwitterAPI =
-        kuon::TwitterAPI::new(api_key, api_secret_key, access_token, access_token_secret).await?;
+    kuon::TwitterAPI::new(api_key, api_secret_key, access_token, access_token_secret).await
+}
 
-    let res = api.tweet("test").await?;
-    println!("{:?}", res);
+#[tokio::main]
+async fn main() -> Result<()> {
+    let api: kuon::TwitterAPI = get_api_client().await?;
+
+    let endpoint = &std::env::var("UJIMARU_API")?;
+    let text = reqwest::get(endpoint).await?.text().await?;
+
+    api.tweet(&text).await?;
+    println!("{}", text);
 
     Ok(())
 }

1999f19a4ba81d60419e549e6093bc2c44e0fe76
hppRC <hpp.ricecake@gmail.com>
2020-07-19 00:10:26 +0000

    🥺chore

--- twitter-client/cronjob.sh
+++ twitter-client/cronjob.sh
@@ -4,4 +4,7 @@ cd ~/ghq/github.com/hppRC/ujimaru/twitter-client
 # source Twitter API tokens and keys
 source .envrc
 
-./target/release/twitter-client
+./target/release/twitter-client || echo "
+`date`
+☠️  ujimaru Run failed ☠️
+" | slackcat --channel misc -t -s -u bot
